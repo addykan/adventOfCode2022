82c7631c5368a06808e30c05380c17a4354f828a
addykan <addykan@gmail.com>
2022-12-08 13:00:02 -0500

    Added starter file

---
+++ template.py
@@ -0,0 +1,19 @@
+
+
+def readFile(path):
+    with open(path, "rt") as f:
+        return f.read()
+
+def part1():
+    data = readFile('input.txt')
+
+def part2():
+    data = readFile('input.txt')
+
+
+def main():
+    print(part1())
+    print(part2())
+
+if __name__ == '__main__':
+    main()
